82b9a9bcc0fc92aec643a08d13eec13d83881ae7
ZiadEhab15 <zeiadehap851@gmail.com>
2024-04-29 12:50:41 +0300

    [Add] Data-Anotation for RegisterDto and LoginDto

--- BisHelpers.Domain/Dtos/LoginDto.cs
+++ BisHelpers.Domain/Dtos/LoginDto.cs
@@ -2,6 +2,10 @@
 
 public class LoginDto
 {
+    [Required]
+    [StringLength(128)]
     public string Email { get; set; } = null!;
+
+    [Required]
     public string Password { get; set; } = null!;
 }
--- BisHelpers.Domain/Dtos/RegisterDto.cs
+++ BisHelpers.Domain/Dtos/RegisterDto.cs
@@ -1,15 +1,27 @@
-﻿namespace BisHelpers.Domain.Dtos;
+﻿using BisHelpers.Domain.Consts;
+
+namespace BisHelpers.Domain.Dtos;
 
 public class RegisterDto
 {
+    [StringLength(200)]
+    [RegularExpression(RegexPatterns.CharactersOnly_Eng)]
     public string FullName { get; set; } = null!;
 
+    [MaxLength(9)]
+    [RegularExpression(RegexPatterns.CollageId)]
     public string CollegeId { get; set; } = null!;
 
+    [Required]
+    [StringLength(128)]
     public string Email { get; set; } = null!;
 
+    [MaxLength(256)]
+    [RegularExpression(RegexPatterns.Password)]
     public string Password { get; set; } = null!;
 
+    [MaxLength(11)]
+    [RegularExpression(RegexPatterns.MobileNumber)]
     public string PhoneNumber { get; set; } = null!;
 
     public DateTime BirthDate { get; set; }
