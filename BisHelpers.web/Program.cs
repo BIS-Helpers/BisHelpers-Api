729c97b15d234b10ff5dd14e979ca1a34263c478
Youssef-AhmedX <youssefahmed1155@gmail.com>
2024-05-05 22:56:17 +0300

    [update] connectionstring

--- BisHelpers.web/Program.cs
+++ BisHelpers.web/Program.cs
@@ -1,20 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
+string? connectionString;
+
+if (builder.Environment.IsDevelopment())
+    connectionString = configuration.GetConnectionString("DefaultConnection");
+else
+    connectionString = configuration.GetConnectionString("ServerConnection");
 
 // Add services to the container.
 builder.Services
     .AddPresentationServices(configuration)
-    .AddInfrastructureServices(configuration)
+    .AddInfrastructureServices(connectionString)
     .AddApplicationServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI();
