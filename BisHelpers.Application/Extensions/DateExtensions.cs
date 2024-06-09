3496cb79328270b3c4ddea143caa1b613304012b
Youssef-AhmedX <youssefahmed1155@gmail.com>
2024-06-09 07:56:39 +0300

    [Update] return level with number only without level

--- BisHelpers.Application/Extensions/DateExtensions.cs
+++ BisHelpers.Application/Extensions/DateExtensions.cs
@@ -11,12 +11,12 @@ public static class DateExtensions
         var yearsDiff = currentDate.Year - dateOfJoin.Year;
 
         if (yearsDiff > 4)
-            return "Level 4";
+            return "4";
 
         if (currentDate.Month >= 9 && currentDate.Month <= 12)
-            return $"Level {yearsDiff + 1}";
+            return $"{yearsDiff + 1}";
 
-        return $"Level {yearsDiff}";
+        return $"{yearsDiff}";
     }
 
     public static string GetAcademicYear(this int year) =>
